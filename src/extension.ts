3c83ada81b3e85b9caa8cf5fb6471bb5bc6e9e85
Daniel Banck <daniel@dbanck.de>
2021-06-17 22:54:20 +0200

    update command, get text selection

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,33 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "jest-diff-parser" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "jest-diff-parser.helloWorld",
+    "jest-diff-parser.parseSelection",
     () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from jest-diff-parser!"
-      );
+      // Get the active text editor
+      const editor = vscode.window.activeTextEditor;
+
+      if (editor) {
+        const document = editor.document;
+        const selection = editor.selection;
+
+        // Get the word within the selection
+        const text = document.getText(selection);
+        if (text.length > 0) {
+          console.log(
+            "🚀 ~ file: extension.ts ~ line 29 ~ activate ~ word",
+            text
+          );
+        }
+
+        // editor.edit(editBuilder => {
+        //   editBuilder.replace(selection, reversed);
+        // });
+      }
     }
   );
 
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
